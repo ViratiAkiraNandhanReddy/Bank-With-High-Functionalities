1536f5fd788061d37ae348de837a741d0724809d
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-01-25 00:25:45 +0530

    feat: Add mail for `event: email verification`

--- src/__mail__/email_verification.py
+++ src/__mail__/email_verification.py
@@ -1,17 +1,20 @@
 from . import *
 
-class welcome:
+class email_verification:
 
-    def __init__(self, receiver_mail_address: str, fullname: str) -> None:
+    def __init__(self, receiver_mail_address: str) -> None:
+
+        self.otp: str = otp_code()
         
-        with open(fr'{DIR_PATH}\src\__mail__\templates\welcome.html') as file:
+        with open(fr'{DIR_PATH}\src\__mail__\templates\email_verification.html') as file:
             
             self.email_html = file.read()\
-                .replace('[TESTER]', fullname)
+                .replace('[CODE]', self.otp)\
+                .replace('[EMAIL]', receiver_mail_address)
         
         self.email = email_msg()
 
-        self.email['Subject'] = 'Welcome to Bank With High Functionalities.'
+        self.email['Subject'] = f'{self.otp} is your code to verify email at Bank With High Functionalities.'
         self.email['From'] = 'Bank With High Functionalities'
         self.email['To'] = receiver_mail_address
 
