5dc67e8be7349bba8f9aa7246f5740cd33e18db4
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-01-26 23:26:21 +0530

    style: Format `src/__mail__/` with `black`

--- src/__mail__/__init__.py
+++ src/__mail__/__init__.py
@@ -1,39 +1,22 @@
-'''
+"""
 Docstring for src.__mail__
-'''
+"""
 
 import os
 import smtplib
 from random import random, randint
 from email.message import EmailMessage as email_msg
 
-DIR_PATH = str(os.environ.get('LOCALAPPDATA')) + r'\Bank-With-High-Functionalities'
+DIR_PATH = str(os.environ.get("LOCALAPPDATA")) + r"\Bank-With-High-Functionalities"
 
-SMTP__MAIL_ADDRESS = os.getenv('SMTP__MAIL_ADDRESS')
-SMTP__APP_PASSWORD = os.getenv('SMTP__APP_PASSWORD')
+SMTP__MAIL_ADDRESS = os.getenv("SMTP__MAIL_ADDRESS")
+SMTP__APP_PASSWORD = os.getenv("SMTP__APP_PASSWORD")
 
-otp_code = lambda: str(
-
-    int( random() * ( 999 - 100 ) + 100 )
-
-    ) + chr(
-
-        randint( 65, 90 )
-
-        ) + str(
-
-            int( random() * ( 99 - 11 ) + 11 )
-
-            ) + chr(
-
-                randint( 65, 90 )
-
-                ) + str(
-
-                    int( random() * ( 99 - 11 ) + 11 )
-
-                    ) + chr(
-
-                        randint( 65, 90 )
-
-                        )
+otp_code = (
+    lambda: str(int(random() * (999 - 100) + 100))
+    + chr(randint(65, 90))
+    + str(int(random() * (99 - 11) + 11))
+    + chr(randint(65, 90))
+    + str(int(random() * (99 - 11) + 11))
+    + chr(randint(65, 90))
+)
