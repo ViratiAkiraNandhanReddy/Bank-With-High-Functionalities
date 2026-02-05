0cc052a32d3db814e492affb1f1c103e5cd6352b
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-02-05 21:17:53 +0530

    chore: Create connection for MySQL

--- src/__server__/JSON.py
+++ src/__server__/JSON.py
@@ -1,2 +1,14 @@
+import pathlib
+from . import DIR_PATH
+
+
 class SERVER:
-    ...
+
+    class traversal:
+
+        def __init__(self) -> None:
+            pass
+
+        def is_user_exists(self, username: str) -> bool:
+
+            return pathlib.Path(rf"{DIR_PATH}\database\json\{username}.json").exists()
--- src/__server__/MySQL.py
+++ src/__server__/MySQL.py
@@ -1,2 +1,22 @@
+import os
+import mysql.connector
+from . import CONFIGURATION_JSON, MYSQL__DB_PASSWORD
+
+# Database Connection
+connection = mysql.connector.connect(
+    host="localhost",
+    password=MYSQL__DB_PASSWORD,
+    user=CONFIGURATION_JSON["MySQL Credentials"]["USERNAME"],
+    database=CONFIGURATION_JSON["MySQL Credentials"]["DATABASE"],
+)
+
+
 class SERVER:
-    ...
+
+    cursor = connection.cursor()
+
+    class table_definition:
+
+        def __init__(self):
+
+            self.cursor = SERVER.cursor
