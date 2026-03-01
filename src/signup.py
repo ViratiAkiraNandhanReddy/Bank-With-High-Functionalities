46d89c6a721037dafed10d128974fc3473dd6dd4
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-03-01 20:01:40 +0530

    style: Format code with black

--- src/signup.py
+++ src/signup.py
@@ -1,6 +1,14 @@
+from . import *
+
+
 class signup_interface:
 
     class signup:
-        
-        def __init__(self):
-            pass
+
+        def __init__(self, parent_window: customtkinter.CTk):
+
+            self.frame__signup = customtkinter.CTkFrame(parent_window, corner_radius=0)
+            self.frame__signup.configure(width=910, height=560)
+
+            self.show_frame = lambda: self.frame__signup.place(x=20, y=20)
+            self.hide_frame = lambda: self.frame__signup.place_forget()
