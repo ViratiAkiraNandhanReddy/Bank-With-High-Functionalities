539aea63c5b9f0416ce0ce08a9c6b2a91b581bcf
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-03-02 17:26:13 +0530

    feat | refactor: Add new banners and icons for login and reset password screens & improved ui/ux

--- src/loadAssets.py
+++ src/loadAssets.py
@@ -6,14 +6,21 @@ DIR_PATH: str = str(os.environ.get("LOCALAPPDATA")) + r"\Bank-With-High-Function
 # --- common --- #
 
 icon__close = Image.open(rf"{DIR_PATH}\assets\icons\material icons\close.png")
+
 icon__more_horiz = Image.open(rf"{DIR_PATH}\assets\icons\material icons\more_horiz.png")
 
 # --- login.py --- #
 
 icon__password = Image.open(rf"{DIR_PATH}\assets\icons\material icons\password.png")
+
 icon__security = Image.open(rf"{DIR_PATH}\assets\icons\material icons\security.png")
+
 icon__account_circle = Image.open(
     rf"{DIR_PATH}\assets\icons\material icons\account_circle.png"
+)
+
+icon__assured_workload = Image.open(
+    rf"{DIR_PATH}\assets\icons\material icons\assured_workload.png"
 )
 
 # --- administrator.py --- #
@@ -23,4 +30,8 @@ icon__developer_mode_tv = Image.open(
 )
 
 
-image_banner__login = Image.open(r"C:\Users\Virati Akira Nandhan Reddy\Downloads\login.jpg")
+image__banner_login = Image.open(rf"{DIR_PATH}\assets\banners\root_banner__login.jpg")
+
+image__banner_reset_password = Image.open(
+    rf"{DIR_PATH}\assets\banners\root_banner__reset_password.jpg"
+)
