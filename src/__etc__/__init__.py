1c82c6f6408b6da2daa00384d23c09cb1f1aa9e3
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-03-29 22:40:45 +0530

    feat: Enhance activation window with dynamic sizing and title label

--- src/__etc__/__init__.py
+++ src/__etc__/__init__.py
@@ -1,1 +1,2 @@
 from .. import *
+from .activation import activation
--- src/__etc__/activation.py
+++ src/__etc__/activation.py
@@ -7,14 +7,19 @@ class activation:
         self._keys = _keys
 
         self.window = customtkinter.CTk()
+
+        _width, _height = 532, 276
+        _x_pos = int((self.window.winfo_screenwidth() / 2) - (_width / 2))
+        _y_pos = int((self.window.winfo_screenheight() / 2) - (_height / 2))
+
         self.window.title("Bank With High Functionalities")
-        self.window.geometry("800x400")
+        self.window.geometry(f"{_width}x{_height}+{_x_pos}+{_y_pos}")
 
         apply_style(self.window, "transparent")
-        title_bar.hide(self.window)
+        title_bar.hide(self.window, no_span=True)
 
-        self.window.minsize(800, 400)
-        self.window.maxsize(800, 400)
+        self.window.minsize(_width, _height)
+        self.window.maxsize(_width, _height)
 
         self.window.bind(
             "<Button-1>",
@@ -22,5 +27,14 @@ class activation:
                 event, self.window
             ),
         )
+
+        customtkinter.CTkLabel(
+            self.window,
+            text="BANK WITH HIGH FUNCTIONALITIES",
+            font=("Segoe UI", 10),
+            bg_color="black",
+            height=0,
+            width=0,
+        ).place(x=5, y=5)
 
         self.window.mainloop()
