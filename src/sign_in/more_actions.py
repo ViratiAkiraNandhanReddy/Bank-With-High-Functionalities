eca9229f5010413bca470d9d670ce7da628ac377
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-05-01 22:27:17 +0530

    style | feat | docs: Add type hints to `src\sign_in\more_actions.py`

--- src/sign_in/more_actions.py
+++ src/sign_in/more_actions.py
@@ -14,30 +14,34 @@ class more_actions_interface:
             parent_frame: customtkinter.CTkFrame,
             parent_window: customtkinter.CTk,
             _callback: Callable,
+            _btn: customtkinter.CTkButton,
         ) -> None:
 
             self.parent_frame: customtkinter.CTkFrame = parent_frame
             self.parent_window: customtkinter.CTk = parent_window
             self.hide_overlay_callback: Callable = _callback
+            self._btn: customtkinter.CTkButton = _btn
 
-            self.internal_frame_00_more_actions = customtkinter.CTkFrame(
-                self.parent_frame,
-                width=306,
-                height=406,
-                border_width=2,
-                border_color="gray",
-                fg_color="transparent",
+            self.internal_frame_00_more_actions: customtkinter.CTkFrame = (
+                customtkinter.CTkFrame(
+                    self.parent_frame,
+                    width=306,
+                    height=406,
+                    border_width=2,
+                    border_color="gray",
+                    fg_color="transparent",
+                )
             )
 
             self.internal_frame_00_more_actions.place(x=724, y=30)
 
-            self.if_00_container_frame_admin_sign_in__flow_sign_in = (
-                customtkinter.CTkFrame(
-                    self.internal_frame_00_more_actions,
-                    width=300,
-                    height=400,
-                    fg_color="transparent",
-                )
+            self.if_00_container_frame_admin_sign_in__flow_sign_in: (
+                customtkinter.CTkFrame
+            ) = customtkinter.CTkFrame(
+                self.internal_frame_00_more_actions,
+                width=300,
+                height=400,
+                fg_color="transparent",
             )
 
             self.if_00_container_frame_admin_sign_in__flow_sign_in.place(x=3, y=3)
@@ -57,17 +61,21 @@ class more_actions_interface:
                 width=0,
             ).place(x=36, y=68)
 
-            self.container_frame__username_admin_sign_in = customtkinter.CTkFrame(
-                self.if_00_container_frame_admin_sign_in__flow_sign_in,
-                width=260,
-                height=40,
-                fg_color="transparent",
-                border_width=1,
-                border_color="#FFFFFF",
-                corner_radius=6,
+            self.container_frame__username_admin_sign_in: customtkinter.CTkFrame = (
+                customtkinter.CTkFrame(
+                    self.if_00_container_frame_admin_sign_in__flow_sign_in,
+                    width=260,
+                    height=40,
+                    fg_color="transparent",
+                    border_width=1,
+                    border_color="#FFFFFF",
+                    corner_radius=6,
+                )
             )
 
-            self.container_frame__username_label_admin_sign_in = customtkinter.CTkLabel(
+            self.container_frame__username_label_admin_sign_in: (
+                customtkinter.CTkLabel
+            ) = customtkinter.CTkLabel(
                 self.if_00_container_frame_admin_sign_in__flow_sign_in,
                 text="username",
                 font=("Roboto", 10),
@@ -88,7 +96,7 @@ class more_actions_interface:
 
             self.container_frame__username_admin_sign_in.place(x=20, y=200)
 
-            self.__username = customtkinter.CTkEntry(
+            self.__username: customtkinter.CTkEntry = customtkinter.CTkEntry(
                 self.container_frame__username_admin_sign_in,
                 placeholder_text="username",
                 width=260 - 40,
@@ -119,17 +127,21 @@ class more_actions_interface:
                 ),
             )
 
-            self.container_frame__password_admin_sign_in = customtkinter.CTkFrame(
-                self.if_00_container_frame_admin_sign_in__flow_sign_in,
-                width=260,
-                height=40,
-                fg_color="transparent",
-                border_width=1,
-                border_color="#FFFFFF",
-                corner_radius=6,
+            self.container_frame__password_admin_sign_in: customtkinter.CTkFrame = (
+                customtkinter.CTkFrame(
+                    self.if_00_container_frame_admin_sign_in__flow_sign_in,
+                    width=260,
+                    height=40,
+                    fg_color="transparent",
+                    border_width=1,
+                    border_color="#FFFFFF",
+                    corner_radius=6,
+                )
             )
 
-            self.container_frame__password_label_admin_sign_in = customtkinter.CTkLabel(
+            self.container_frame__password_label_admin_sign_in: (
+                customtkinter.CTkLabel
+            ) = customtkinter.CTkLabel(
                 self.if_00_container_frame_admin_sign_in__flow_sign_in,
                 text="password",
                 font=("Roboto", 10),
@@ -163,7 +175,7 @@ class more_actions_interface:
                 command=self.forgot_administrator_password,
             ).place(x=182, y=291)
 
-            self.__password = customtkinter.CTkEntry(
+            self.__password: customtkinter.CTkEntry = customtkinter.CTkEntry(
                 self.container_frame__password_admin_sign_in,
                 placeholder_text="password",
                 width=260 - 40,
@@ -195,29 +207,44 @@ class more_actions_interface:
                 ),
             )
 
-            administrator_sign_in_btn = customtkinter.CTkButton(
-                self.if_00_container_frame_admin_sign_in__flow_sign_in,
-                text="Sign in",
-                width=260,
-                height=40,
-                border_width=0,
-                text_color="#000000",
-                bg_color="transparent",
-                fg_color="#818181",
-                font=("Roboto", 16, "bold"),
-                hover_color="#929090",
-                corner_radius=6,
-                command=self.validate_administrator_password,
+            administrator_sign_in_btn: customtkinter.CTkButton = (
+                customtkinter.CTkButton(
+                    self.if_00_container_frame_admin_sign_in__flow_sign_in,
+                    text="Sign in",
+                    width=260,
+                    height=40,
+                    border_width=0,
+                    text_color="#000000",
+                    bg_color="transparent",
+                    fg_color="#818181",
+                    font=("Roboto", 16, "bold"),
+                    hover_color="#929090",
+                    corner_radius=6,
+                    command=self.validate_administrator_password,
+                )
             )
             administrator_sign_in_btn.place(x=20, y=340)
 
-        def forgot_administrator_password(self):
+        def forgot_administrator_password(self) -> None:
             self.if_00_container_frame_admin_sign_in__flow_sign_in.place_forget()
+
+            self.if_01_container_frame_admin_sign_in__flow_forgot_admin_password: (
+                customtkinter.CTkFrame
+            ) = customtkinter.CTkFrame(
+                self.internal_frame_00_more_actions,
+                width=300,
+                height=400,
+                fg_color="transparent",
+            )
+
+            self.if_01_container_frame_admin_sign_in__flow_forgot_admin_password.place(
+                x=3, y=3
+            )
 
         def validate_administrator_password(self) -> None:
 
-            admin_username = self.__username.get().strip()
-            admin_password = self.__password.get().strip()
+            admin_username: str = self.__username.get().strip()
+            admin_password: str = self.__password.get().strip()
 
             self.__username.bind(
                 "<KeyPress>",
@@ -339,15 +366,11 @@ class more_actions_interface:
                 return
 
             else:
-                self.parent_window.withdraw()
-                self.hide_overlay_callback()
-                self.toplevel__admin_dashboard = administrator_interface.administrator(
-                    self.parent_window
+                self.overlay_frame__admin_dashboard = (
+                    administrator_interface.administrator(self.parent_window, self._btn)
                 )
-                self.parent_window.wait_window(
-                    self.toplevel__admin_dashboard.window__developer
-                )
-                self.parent_window.deiconify()
+                self.overlay_frame__admin_dashboard.show_frame()
+                self.parent_frame.after(800, self.hide_overlay_callback)
 
     class more_actions:
 
@@ -355,7 +378,7 @@ class more_actions_interface:
             self, parent_window: customtkinter.CTk, _btn: customtkinter.CTkButton
         ) -> None:
 
-            self.frame__more_actions = customtkinter.CTkFrame(
+            self.frame__more_actions: customtkinter.CTkFrame = customtkinter.CTkFrame(
                 parent_window, corner_radius=0
             )
             self.frame__more_actions.configure(width=1060, height=610)
@@ -370,5 +393,5 @@ class more_actions_interface:
             )
 
             more_actions_interface.frame__administrator(
-                self.frame__more_actions, parent_window, self.hide_frame
+                self.frame__more_actions, parent_window, self.hide_frame, _btn
             )
