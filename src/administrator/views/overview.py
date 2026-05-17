3412f6dd0d1cc1d06451c9c17a7145da0c72710b
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-05-17 21:49:22 +0530

    feat: Enhance administrator interface with username handling and improve frame configurations

---
+++ src/administrator/views/overview.py
@@ -0,0 +1,13 @@
+from .. import customtkinter, Callable
+
+
+class overview:
+
+    def __init__(self, parent_frame: customtkinter.CTkFrame) -> None:
+
+        self.frame__overview: customtkinter.CTkFrame = customtkinter.CTkFrame(
+            parent_frame, width=870, height=590, fg_color="#0a0a0a"
+        )
+
+        self.show_frame: Callable = lambda: self.frame__overview.place(x=220, y=50)
+        self.hide_frame: Callable = lambda: self.frame__overview.place_forget()
