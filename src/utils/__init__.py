9321dc7a1b3e2cd0e571b79f5a485e798639d0b2
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-06-01 00:44:04 +0530

    feat: Add root path definition to utils module

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,15 @@
+from os import environ
+from pathlib import Path
+
 from .theme import colors
 from .browser import browser
 from .privacy import masking
 from .network import connection
 
+root: Path = Path(environ.get("LOCALAPPDATA", "")) / "Bank-With-High-Functionalities"
+
 __all__ = [
+    "root",
     "colors",
     "browser",
     "masking",
