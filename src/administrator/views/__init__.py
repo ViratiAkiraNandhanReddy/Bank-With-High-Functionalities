8e6f0c6d84f8a75a1decb24c4208ddfcfe955044
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-06-03 22:14:34 +0530

    feat: Refactor navigation to include users and access, issues and broadcasts views, and add campaign icon

--- src/administrator/views/__init__.py
+++ src/administrator/views/__init__.py
@@ -1,5 +1,6 @@
-from .users import users
 from .overview import overview
 from .settings import settings
+from .users_and_access import users_and_access
+from .issues_and_broadcasts import issues_and_broadcasts
 
-__all__ = ["overview", "users", "settings"]
+__all__ = ["overview", "users_and_access", "issues_and_broadcasts", "settings"]
