d9e0cb80b776100b9288c1d98733952234f29d43
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-07-02 23:29:46 +0530

    refactor(dashboard): update dashboard initialization and frame handling

--- src/dashboard/__init__.py
+++ src/dashboard/__init__.py
@@ -8,10 +8,25 @@ class dashboard_interface:
 
     class dashboard:
 
-        def __init__(self, username: str, parent: customtkinter.CTk) -> None:
-            self.username = username
+        def __init__(
+            self,
+            parent_window: customtkinter.CTk,
+            _btn: customtkinter.CTkButton,
+            username: str,
+        ) -> None:
 
-            self.window__dashboard = customtkinter.CTkToplevel(parent)
+            self.frame__dashboard: customtkinter.CTkFrame = customtkinter.CTkFrame(
+                parent_window, width=1100, height=650, corner_radius=0, fg_color="black"
+            )
+
+            self.show_frame: Callable = lambda: (
+                self.frame__dashboard.place(x=0, y=0),
+                _btn.place_forget(),
+            )
+            self.hide_frame: Callable = lambda: (
+                self.frame__dashboard.place_forget(),
+                _btn.place(x=1080, y=0),
+            )
 
     class transactions:
 
