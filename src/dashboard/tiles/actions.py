cfa4387c395f52cb42aae915b511fc42ab9bb52b
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-04 18:56:49 +0530

    feat(actions): add actions class with customtkinter frames for user dashboard

---
+++ src/dashboard/tiles/actions.py
@@ -0,0 +1,19 @@
+from ... import customtkinter
+from .balance import balance
+
+
+class actions:
+
+    def __init__(self, parent_frame: customtkinter.CTkFrame) -> None:
+
+        self.balance = balance(parent_frame)
+
+        self.frame__actions: customtkinter.CTkFrame = customtkinter.CTkFrame(
+            parent_frame, width=350, height=40, fg_color="#0a0a0a"
+        )
+        self.frame__actions.place(x=740, y=200)
+
+        self.frame__action_view: customtkinter.CTkFrame = customtkinter.CTkFrame(
+            parent_frame, width=350, height=390, fg_color="#0a0a0a"
+        )
+        self.frame__action_view.place(x=740, y=250)
