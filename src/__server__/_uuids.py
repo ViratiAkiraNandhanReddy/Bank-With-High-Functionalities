cbbe39f1d1aaf1a0eff744a0dcc56636968edfb3
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-06 23:24:39 +0530

    refactor(uuid): simplify UUID validation and generation methods

--- src/__server__/_uuids.py
+++ src/__server__/_uuids.py
@@ -3,21 +3,20 @@ import uuid
 
 class _uuids:
 
-    UUID_NAMESPACE_BWHF = uuid.UUID("71da99dc-ce4e-575e-a319-3083e9265046")
-    # pre-generated using:
-    # uuid.uuid5(
-    #     uuid.NAMESPACE_URL,
-    #     "https://viratiakiranandhanreddy.github.io/Bank-With-High-Functionalities/",
-    # )
+    @classmethod
+    def generate(cls) -> str:
+
+        return str(uuid.uuid4())
 
     @classmethod
-    def generate_uuid5(cls, name: str) -> str:
-        return str(uuid.uuid5(cls.UUID_NAMESPACE_BWHF, name))
+    def validate(cls, value: str) -> bool:
 
-    @classmethod
-    def validate_uuid5(cls, uuid_to_validate: str) -> bool:
         try:
-            uuid_obj = uuid.UUID(uuid_to_validate, version=5)
-            return True
+
+            uuid_obj = uuid.UUID(value)
+
+            return uuid_obj.version == 4
+
         except ValueError:
+
             return False
