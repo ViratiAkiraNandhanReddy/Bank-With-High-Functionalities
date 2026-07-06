0140b267edbf14cb9aa107c3c2fed5651a283e64
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-06 16:20:17 +0530

    feat(balance): integrate SERVER for dynamic balance display in dashboard

--- src/dashboard/tiles/balance.py
+++ src/dashboard/tiles/balance.py
@@ -1,4 +1,4 @@
-from ... import customtkinter
+from ... import customtkinter, SERVER
 
 
 class balance:
@@ -9,3 +9,17 @@ class balance:
             parent_frame, width=500, height=50, fg_color="#0a0a0a"
         )
         self.frame__balance.place(x=10, y=50)
+
+        self.label__balance: customtkinter.CTkLabel = customtkinter.CTkLabel(
+            self.frame__balance,
+            text=f"$ {SERVER.lookup.user.balance(username):,.2f}",
+            width=480,
+            height=50,
+            text_color="#FFFFFF",
+            font=("Roboto", 18),
+        )
+        self.label__balance.place(x=10, y=0)
+
+        self.refresh = lambda: self.label__balance.configure(
+            text=f"$ {SERVER.lookup.user.balance(username):,.2f}"
+        )
