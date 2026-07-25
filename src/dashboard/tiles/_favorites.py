72e8301b79cf45c507b8af98f690f2ddc3ea9121
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-25 19:25:44 +0530

    feat: implement card loading of frequent transfer recipients in favorites

--- src/dashboard/tiles/_favorites.py
+++ src/dashboard/tiles/_favorites.py
@@ -1,11 +1,28 @@
-from ... import customtkinter
+from ... import customtkinter, assets, SERVER
 
 
 class favorites:
 
     def __init__(self, parent_frame: customtkinter.CTkFrame, username: str) -> None:
 
+        self.username = username
+
         self.frame__favorites: customtkinter.CTkFrame = customtkinter.CTkFrame(
             parent_frame, width=245, height=220, fg_color="#0a0a0a"
         )
         self.frame__favorites.place(x=10, y=420)
+
+        self.load_favorites_cards()
+
+    def load_favorites_cards(self) -> None:
+
+        frequent_transfer_recipients: list[tuple[str, int]] = (
+            SERVER.lookup.user.frequent_transfer_recipients(self.username)
+        )
+
+        for n, (recipient, count) in enumerate(frequent_transfer_recipients):
+
+            card = customtkinter.CTkFrame(
+                self.frame__favorites, width=231, height=64, fg_color="#111111"
+            )
+            card.place(x=7, y=(7 + n * (64 + 7)))
