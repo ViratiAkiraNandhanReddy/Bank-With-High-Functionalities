69f6c24583ce049065935abe718bb7b4f7c36d1a
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-28 18:20:16 +0530

    feat: rename messages class to notice and update imports accordingly

--- src/dashboard/tiles/_notice.py
+++ src/dashboard/tiles/_notice.py
@@ -1,11 +1,11 @@
 from ... import customtkinter
 
 
-class messages:
+class notice:
 
     def __init__(self, parent_frame: customtkinter.CTkFrame, username: str) -> None:
 
-        self.frame__messages: customtkinter.CTkFrame = customtkinter.CTkFrame(
+        self.frame__notice: customtkinter.CTkFrame = customtkinter.CTkFrame(
             parent_frame, width=570, height=140, fg_color="#0a0a0a"
         )
-        self.frame__messages.place(x=520, y=50)
+        self.frame__notice.place(x=520, y=50)
