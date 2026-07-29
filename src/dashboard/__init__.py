6370a8062600d3336b9c290fc125836f4aebc429
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-29 21:34:44 +0530

    feat: update dashboard to use notice class and display current notice content

--- src/dashboard/__init__.py
+++ src/dashboard/__init__.py
@@ -125,7 +125,7 @@ class dashboard_interface:
 
             self.actions = actions(self.frame__dashboard, username)
 
-            self.messages = messages(self.frame__dashboard, username)
+            self.notice = notice(self.frame__dashboard, username)
 
             self.security = security(self.frame__dashboard, username)
 
--- src/dashboard/tiles/_notice.py
+++ src/dashboard/tiles/_notice.py
@@ -1,4 +1,4 @@
-from ... import customtkinter
+from ... import customtkinter, SERVER
 
 
 class notice:
@@ -9,3 +9,16 @@ class notice:
             parent_frame, width=570, height=140, fg_color="#0a0a0a"
         )
         self.frame__notice.place(x=520, y=50)
+
+        self.notice_content: str = SERVER.lookup.application.current_notice()
+
+        self.label__notice: customtkinter.CTkLabel = customtkinter.CTkLabel(
+            self.frame__notice,
+            text=self.notice_content,
+            width=550,
+            wraplength=540,
+            justify="left",
+            anchor="nw",
+        )
+        self.label__notice.place(x=10, y=10)
+
