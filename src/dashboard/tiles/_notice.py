6b4f87a99ef6c53555860d5bfa67478c796c79cc
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-30 16:30:33 +0530

    feat: rename notice class to announcement and update related variables

--- src/dashboard/tiles/_notice.py
+++ src/dashboard/tiles/_notice.py
@@ -1,18 +1,18 @@
 from ... import customtkinter, SERVER
 
 
-class notice:
+class announcement:
 
     def __init__(self, parent_frame: customtkinter.CTkFrame, username: str) -> None:
 
-        self.frame__notice: customtkinter.CTkFrame = customtkinter.CTkFrame(
+        self.frame__announcement: customtkinter.CTkFrame = customtkinter.CTkFrame(
             parent_frame, width=570, height=140, fg_color="#0a0a0a"
         )
-        self.frame__notice.place(x=520, y=50)
+        self.frame__announcement.place(x=520, y=50)
 
-        self.notice_content: str = SERVER.lookup.application.current_notice()
+        self.announcement_content: str = SERVER.lookup.application.current_announcement()
 
-        if self.notice_content == "no new notices":
+        if self.announcement_content == "no new announcement":
 
             justify: str = "center"
             anchor: str = "center"
@@ -22,14 +22,14 @@ class notice:
             justify: str = "left"
             anchor: str = "nw"
 
-        self.label__notice: customtkinter.CTkLabel = customtkinter.CTkLabel(
-            self.frame__notice,
-            text=self.notice_content,
+        self.label__announcement: customtkinter.CTkLabel = customtkinter.CTkLabel(
+            self.frame__announcement,
+            text=self.announcement_content,
             width=550,
-            height=120,
+            height=126,
             wraplength=550,
             justify=justify,
             anchor=anchor,
             font=("Consolas", 13),
         )
-        self.label__notice.place(x=10, y=7)
+        self.label__announcement.place(x=10, y=7)
