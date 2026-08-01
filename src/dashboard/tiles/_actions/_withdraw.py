04b97198464f0f1bcef33ab06ed64f8c7dd6aa5a
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-08-01 23:31:40 +0530

    feat: add label for withdraw funds section in the UI

--- src/dashboard/tiles/_actions/_withdraw.py
+++ src/dashboard/tiles/_actions/_withdraw.py
@@ -27,3 +27,20 @@ class withdraw:
         self.show_frame: Callable = lambda: self.frame__withdraw.place(x=740, y=250)
 
         self.hide_frame: Callable = lambda: self.frame__withdraw.place_forget()
+
+        customtkinter.CTkLabel(
+            self.frame__withdraw,
+            text="Withdraw Funds",
+            font=("Consolas", 16, "bold"),
+            text_color="#FFFFFF",
+            image=customtkinter.CTkImage(
+                light_image=assets.icons.material.do_not_disturb_on,
+                dark_image=assets.icons.material.do_not_disturb_on,
+                size=(42, 42),
+            ),
+            compound="top",
+            height=0,  # 61
+            width=0,  # 126
+        ).place(
+            x=112, y=44
+        )  # x = 112 ; y = 44.5
