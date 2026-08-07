f139e63aec00114ae4e7234b0d2e008469b07143
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-08-08 00:27:02 +0530

    feat(account-info): add UUID and Created At labels to account info tile
    Co-authored-by: Dupati Prajwal <dupatiprajwal@gmail.com>

--- src/dashboard/tiles/_account_info.py
+++ src/dashboard/tiles/_account_info.py
@@ -21,3 +21,15 @@ class account_info:
                 size=(100, 100),
             ),
         ).place(x=11, y=3)
+
+        customtkinter.CTkLabel(
+            self.frame__account_info,
+            text=f"UUID: {SERVER.lookup.user.resolve_uuid(username)}",
+            font=("Consolas", 15, "bold"),
+        ).place(x=122, y=20)
+
+        customtkinter.CTkLabel(
+            self.frame__account_info,
+            text=f"Created At: {SERVER.lookup.user.created_at(username)}",
+            font=("Consolas", 15, "bold"),
+        ).place(x=122, y=55)
