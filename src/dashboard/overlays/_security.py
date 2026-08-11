2d7b56f18dbfdfd641e13e34029e76c1d107663d
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-08-12 01:10:53 +0530

    style(security-overlay): clean up formatting and whitespace in security overlay code

--- src/dashboard/overlays/_security.py
+++ src/dashboard/overlays/_security.py
@@ -23,8 +23,13 @@ class security_overlay:
 
         self.username = username
 
-        self.frame__security: customtkinter.CTkScrollableFrame = customtkinter.CTkScrollableFrame(
-            parent_frame, width=1058, height=578, fg_color="#0a0a0a",
+        self.frame__security: customtkinter.CTkScrollableFrame = (
+            customtkinter.CTkScrollableFrame(
+                parent_frame,
+                width=1058,
+                height=578,
+                fg_color="#0a0a0a",
+            )
         )
 
         self.show_frame: Callable = lambda: (
@@ -34,24 +39,24 @@ class security_overlay:
         self.hide_frame: Callable = lambda: self.frame__security.place_forget()
 
     def load_security_cards(self) -> None:
-    
+
         total_security_events: list[tuple[str, str]] = (
             SERVER.lookup.security_event.recent(self.username, -1)
         )
 
         columns = 4
-        
+
         for column in range(columns):
-        
+
             self.frame__security.grid_columnconfigure(
                 column,
                 weight=1,
             )
-    
+
         for n, (event_type, timestamp) in enumerate(total_security_events):
-    
+
             get_event_labels = EVENT_LABELS.get(event_type, ("Unknown", "[INFO]"))
-    
+
             card = customtkinter.CTkFrame(
                 self.frame__security,
                 width=252,
@@ -69,7 +74,7 @@ class security_overlay:
                 pady=5,
                 sticky="nsew",
             )
-    
+
             customtkinter.CTkLabel(
                 card,
                 text=get_event_labels[0],
@@ -79,7 +84,7 @@ class security_overlay:
                 text_color="#D4D4D4",
                 anchor="w",
             ).place(x=6, y=0)
-    
+
             customtkinter.CTkLabel(
                 card,
                 text=get_event_labels[1],
@@ -89,7 +94,7 @@ class security_overlay:
                 text_color="#D4D4D4",
                 anchor="e",
             ).place(x=159, y=0)
-    
+
             customtkinter.CTkLabel(
                 card,
                 text=(
@@ -103,11 +108,11 @@ class security_overlay:
                 font=("Consolas", 10),
                 text_color="#A3A3A3",
             ).place(x=4, y=24)
-    
+
     def refresh(self) -> None:
-    
+
         for widget in self.frame__security.winfo_children():
-    
+
             widget.destroy()
-    
+
         self.load_security_cards()
