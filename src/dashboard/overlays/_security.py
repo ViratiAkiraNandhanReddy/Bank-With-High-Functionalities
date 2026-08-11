4cddfe2aa9db396c2b6e5a114cfde0eae12afafa
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-08-12 01:09:52 +0530

    feat(security-overlay): implement load_security_cards method to display security events

--- src/dashboard/overlays/_security.py
+++ src/dashboard/overlays/_security.py
@@ -33,6 +33,81 @@ class security_overlay:
         )
         self.hide_frame: Callable = lambda: self.frame__security.place_forget()
 
-    def refresh(self) -> None:
+    def load_security_cards(self) -> None:
+    
+        total_security_events: list[tuple[str, str]] = (
+            SERVER.lookup.security_event.recent(self.username, -1)
+        )
 
-        pass
+        columns = 4
+        
+        for column in range(columns):
+        
+            self.frame__security.grid_columnconfigure(
+                column,
+                weight=1,
+            )
+    
+        for n, (event_type, timestamp) in enumerate(total_security_events):
+    
+            get_event_labels = EVENT_LABELS.get(event_type, ("Unknown", "[INFO]"))
+    
+            card = customtkinter.CTkFrame(
+                self.frame__security,
+                width=252,
+                height=38,
+                fg_color="#111111",
+            )
+
+            column = n % columns
+            row = n // columns
+
+            card.grid(
+                row=row,
+                column=column,
+                padx=5,
+                pady=5,
+                sticky="nsew",
+            )
+    
+            customtkinter.CTkLabel(
+                card,
+                text=get_event_labels[0],
+                width=153,
+                height=24,
+                font=("Consolas", 12, "bold"),
+                text_color="#D4D4D4",
+                anchor="w",
+            ).place(x=6, y=0)
+    
+            customtkinter.CTkLabel(
+                card,
+                text=get_event_labels[1],
+                width=89,
+                height=24,
+                font=("Consolas", 12, "bold"),
+                text_color="#D4D4D4",
+                anchor="e",
+            ).place(x=159, y=0)
+    
+            customtkinter.CTkLabel(
+                card,
+                text=(
+                    datetime.fromisoformat(timestamp)
+                    .replace(tzinfo=timezone.utc)
+                    .astimezone()
+                    .strftime("%Y-%m-%d %I:%M:%S %p (%z)")
+                ),  # %Y-%m-%d %I:%M:%S %p (%z)
+                width=244,
+                height=0,  # 12
+                font=("Consolas", 10),
+                text_color="#A3A3A3",
+            ).place(x=4, y=24)
+    
+    def refresh(self) -> None:
+    
+        for widget in self.frame__security.winfo_children():
+    
+            widget.destroy()
+    
+        self.load_security_cards()
